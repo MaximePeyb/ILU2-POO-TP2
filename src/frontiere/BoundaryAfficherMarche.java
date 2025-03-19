8adfc48982626a4bd06b3981e3d24da84a4c0a86
Maxime Peybernes <maxime.peybernes@gmail.com>
2025-03-19 11:31:45 +0100

    TP2 7

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,18 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] dataMarche = controlAfficherMarche.donnerInfosMarche();
+		if (dataMarche.length==0) 
+			System.out.println("Le marché est vide, revenez plus tard.");
+		else {
+			System.out.println(nomAcheteur+", vous trouverez au marché :");
+			for(int i=0;i<dataMarche.length;) {
+				String vendeur = dataMarche[i++];
+				String qt = dataMarche[i++];
+				String produit = dataMarche[i++];
+				
+				System.out.println("- "+vendeur+" qui vend "+ qt+ " "+produit);
+			}
+		}
 	}
 }
